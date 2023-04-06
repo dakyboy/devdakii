02fd6bdb81902716f02385bbf96eb5bdc682635b
Aaron Edaku <eaaron25@gmail.com>
2023-04-06 13:58:10 +0000

    Setup page layout including app bar

---
+++ resources/views/components/content/header.blade.php
@@ -0,0 +1,58 @@
+<header>
+    <nav class="nav-container">
+        <a>
+            <div>
+                <p>Dev Dakii</p>
+            </div>
+        </a>
+        <a>
+            <div class="nav-links">
+                <a href="#best-sellers-title">
+                    <p>Work</p>
+                </a>
+                <a href="#about-us-title">
+                    <p>About</p>
+                </a>
+                <a href="#about-us-title">
+                    <p>Contact</p>
+                </a>
+            </div>
+        </a>
+    </nav>
+</header>
+
+<style>
+    header {
+        width: 100%;
+        padding: .1rem;
+        display: flex;
+        flex-flow: column;
+    }
+
+    .nav-container {
+        position: sticky;
+        top: 0;
+        width: 100%;
+        flex: 1;
+        padding: 2rem;
+        margin: 0 auto;
+        display: flex;
+        flex-flow: row wrap;
+        align-items: center;
+        justify-content: space-between;
+    }
+
+    .nav-links {
+        display: flex;
+    }
+
+    .nav-links a {
+        padding: 1rem;
+        color: white;
+    }
+
+    .nav-links a:hover {
+        color: rgb(209, 211, 212);
+        box-shadow: 
+    }
+</style>
---
+++ resources/views/dev_dakii.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <x-content.page-description>
+        <x-slot:title>Dev Dakii: Full stack developer </x-slot:title>
+        <x-slot:description>Dev Dakii is a skilled full stack developer based in Jinja, Uganda, with expertise in
+            front-end and back-end web and mobile development. Get professional web solutions for your business with Dev
+            Dakii.
+            Contact now!</x-slot:description>
+    </x-content.page-description>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,100&display=swap"
+        rel="stylesheet">
+
+    <style>
+        /*Reset*/
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+
+        }
+
+        html {
+            font-family: 'Roboto', sans-serif;
+            min-height: 100%;
+            font-size: 62.5%;
+            min-width: 90%;
+            margin: 0 auto;
+
+        }
+
+        body {
+            font-size: 1.6rem;
+            min-height: 100vh;
+            min-width: 300px;
+            margin: 0vh auto;
+            background-color: rgb(3, 70, 148);
+            color: #ffffff;
+        }
+
+        .app-container {
+            display: flex;
+            flex-flow: column wrap;
+            justify-content: space-between;
+            align-items: center;
+        }
+    </style>
+
+</head>
+
+<body class="app-container">
+<x-content.header ></x-content.header>
+</body>
+
+</html>
